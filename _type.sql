d83fd9f8fbd8a999cefc01a07568b6e396607bd8
Drew Crampsie <me@drewc.ca>
2015-12-14 12:09:32 -0800

    Add the _type basics.

---
+++ _type.sql
@@ -0,0 +1,5 @@
+CREATE SCHEMA IF NOT EXISTS pongo;
+SET search_path TO pongo,public;
+CREATE OR REPLACE FUNCTION "_type"(anyelement) RETURNS json AS $$
+  SELECT to_json(pg_typeof($1)) AS "type" ;
+$$ LANGUAGE SQL;
